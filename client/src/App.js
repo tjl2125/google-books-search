aa506c5d83b05514432d81562e1af20a7876d5c7
Taylor Leong <tjl2125@alum.barnard.edu>
2021-06-29 02:44:09 -0400

    savebtn and search components done

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Saved from "./pages/Saved"; 
-// import Books from "./pages/Books";
-// import Detail from "./pages/Detail";
-// import NoMatch from "./pages/NoMatch";
-// import Nav from "./components/Nav";
+
 
 function App() {
   return (
