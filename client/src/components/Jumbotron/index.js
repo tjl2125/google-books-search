2babc1c9539d21b901cafe2917ca6423d479c4e3
Taylor Leong <tjl2125@alum.barnard.edu>
2021-06-29 02:35:51 -0400

    setting up components and pages finished jumbotron and grid

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -4,8 +4,9 @@ function Jumbotron({ children }) {
   return (
     <div
       style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron"
-    >
+      className="jumbotron">
+        <h2>Google Books Search</h2>
+        <h3>Feel free to search for books and save books for later use</h3>
       {children}
     </div>
   );
