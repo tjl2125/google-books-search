b39bcbadc0e0f64b6bec8875c4a286a7605ae1f0
Taylor Leong <tjl2125@alum.barnard.edu>
2021-06-29 02:47:04 -0400

    finishing SearchList and Item

---
+++ client/src/components/SearchList/SearchList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import SearchList from "../SearchListItem/SearchListItem";
+
+class SearchList extends Component {
+
+    render() {
+        console.log("Props", this.props.bookState)
+        return (
+            this.props.bookState.map((book) => (
+            <SearchList 
+            key={book.id}
+            id={book.id}
+            title={book.volumeInfo.title}
+            link={book.volumeInfo.previewLink}
+            authors={book.volumeInfo.authors && book.volumeInfo.authors.length > 1 ? book.volumeInfo.authors.join(", ") : book.volumeInfo.authors}
+            image={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "https://previews.123rf.com/images/pavelstasevich/pavelstasevich1811/pavelstasevich181101065/112815953-no-image-available-icon-flat-vector.jpg"}
+            description={book.volumeInfo.description}
+            saveGoogleBook={this.props.saveGoogleBook}
+            />
+        ))
+        )
+                           
+    }
+}
+
+export default SearchList; 
